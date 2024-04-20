e9958055ceac2888d7824623f7de7c88feb216a0
MenyUTT <122386195+MenyUTT@users.noreply.github.com>
2024-04-19 21:40:32 -0600

    Cruds Terminados

--- ProyectoDesarrollo/Data/ApplicationDbContext.cs
+++ ProyectoDesarrollo/Data/ApplicationDbContext.cs
@@ -8,15 +8,26 @@ namespace ProyectoDesarrollo.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+        public DbSet<Warehouses> warehouses { get; set; }
+        public DbSet<Countries> countries { get; set; }
 
         public DbSet<Customers> customers { get; set; }
         public DbSet<Contacts> contacts { get; set; }
         public DbSet<Orders> orders { get; set; }
         public DbSet<Products> products { get; set; }
         public DbSet<Product_Categories> product_categories { get; set; }
+        public DbSet<Regions> regions { get; set; }
+        public DbSet<Inventories> inventories { get; set; }
 
+        public DbSet<Locations> locations { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Inventories>()
+              .HasOne(o => o.warehouses)
+              .WithMany()
+              .HasForeignKey(o => o.WAREHOUSE_ID);
+
+
             modelBuilder.Entity<Orders>()
                .HasOne(o => o.Customer)
                .WithMany()
@@ -26,6 +37,11 @@ namespace ProyectoDesarrollo.Data
                .HasOne(o => o.Customer)
                .WithMany()
                .HasForeignKey(o => o.CUSTOMER_ID);
+
+            modelBuilder.Entity<Countries>()
+              .HasOne(o => o.Regions)
+              .WithMany()
+              .HasForeignKey(o => o.REGION_ID);
 
             modelBuilder.Entity<Products>()
               .HasOne(o => o.Categories)
